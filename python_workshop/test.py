eb15c01bd77394bed13bdcb650d5f03b53168a41
David Yoon <david_yoon86@hotmail.com>
2015-12-04 16:48:58 -0500

    sorted

--- python_workshop/test.py
+++ python_workshop/test.py
@@ -35,4 +35,32 @@ print "The top 5 songs were:"
 for song in top_5_songs:
 	print "{} {}".format(song["Song Clean"], song["PlayCount"])
 
+
+rows = [row["ARTIST CLEAN"] for row in rows]
+artist_freq = {}
+for x in rows:
+	if x in artist_freq:
+		artist_freq[x] += 1
+	else:
+		artist_freq[x] = 1	
+
+top_artists = sorted(artist_freq.items(), key=lambda x: x[1], reverse=True)
+
+top_10_artists = top_artists[:10]
+
+for pair in top_10_artists:
+	print "{} {}".format(pair[0], pair[1])
+
 import pdb; pdb.set_trace()
+
+
+
+
+
+
+
+
+
+
+
+
