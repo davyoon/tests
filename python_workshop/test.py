89885d6ba4a0c25dcaa38289b00f7874119cd2fd
David Yoon <david_yoon86@hotmail.com>
2015-12-04 16:24:01 -0500

    python exercise

---
+++ python_workshop/test.py
@@ -0,0 +1,38 @@
+import csv
+csv_file = open("rock.csv", 'rb')  #if python 3, use 'rU'
+
+reader = csv.DictReader(csv_file)
+
+rows = [row for row in reader]
+
+def is_valid_year(string):
+	try:
+		int(string)
+	except ValueError:
+		return False
+	else:
+		return int(string) > 1900
+
+
+print "There were {} songs released in 1981".format(
+	len([x for x in rows if x["Release Year"] == "1981"])
+)
+
+print "There were {} songs released before 1984.".format(
+	len([x for x in rows if is_valid_year(x["Release Year"]) and int(x["Release Year"]) < 1984])
+)
+
+print "The earliest release year is {}".format(
+	min([row["Release Year"] for row in rows if is_valid_year(row["Release Year"])])
+)
+
+top_5_songs = sorted(
+	rows, 
+	key=lambda row: int(row["PlayCount"]), 
+	reverse=True)[:5]
+
+print "The top 5 songs were:"
+for song in top_5_songs:
+	print "{} {}".format(song["Song Clean"], song["PlayCount"])
+
+import pdb; pdb.set_trace()
