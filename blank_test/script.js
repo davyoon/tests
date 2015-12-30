c200fbaea24b61ef139361447306976596c89a9c
David Yoon <david_yoon86@hotmail.com>
2015-12-29 20:44:55 -0500

    blank test

---
+++ blank_test/script.js
@@ -0,0 +1,5 @@
+var person = {};
+person.firstname = "david"
+person.address = {};
+person.address.street = "208st";
+console.log(person.address.street)
--- fourSquare_test/server.js
+++ fourSquare_test/server.js
@@ -17,10 +17,12 @@ var secret = JSON.parse(fs.readFileSync("client_secret.json", "utf8")).secret;
 
 app.get('/', function(req, res) {
 
-	request("https://api.foursquare.com/v2/venues/search?client_id=" + id + "&client_secret=" + secret + "&near=NewYork,NY&query=sushi&v=20140806&m=swarm", function(err, response, body){
+	request("https://api.foursquare.com/v2/venues/explore?client_id=" + id + "&client_secret=" + secret + "&ll=40.7593910,-73.7755910&section=food&limit=50&v=20140806&m=swarm", function(err, response, body){
 		var data = JSON.parse(body);
-		var restaurants = data.response.venues;
+		var restaurants = data.response.groups[0].items;
+		console.log(restaurants)
 		res.render("index.html.ejs", {restaurants: restaurants})
+
 	});
 });
 
